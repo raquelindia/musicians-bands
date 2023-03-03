a78b1cfbf9ca81ed41b99fa8afd83b3619a09264
raquelindia <raquelicruz94@gmail.com>
2023-03-03 16:15:15 -0500

    added band and musician model, coded sequelize connection

--- Band.js
+++ Band.js
@@ -1,7 +1,12 @@
 const {Sequelize, sequelize} = require('./db');
 
 // TODO - define the Band model
-let Band;
+
+let Band = sequelize.define('band', {
+    name: Sequelize.STRING,
+    genre: Sequelize.STRING
+    
+});
 
 module.exports = {
     Band
--- db.js
+++ db.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const { Sequelize, Model } = require('sequelize');
-
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: path.join(__dirname, 'db.sqlite')
+});
 // TODO - create the new sequelize connection
 
 module.exports = {
